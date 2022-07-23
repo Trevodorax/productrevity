589507617c64ae768a752a3a39083e98ba9a6e3b
Olua Parrot <phvcgdx@gmail.com>
2022-07-23 18:37:41 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./Header.js";
+import Body from "./Body.js";
+import Footer from "./Footer.js";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Body.js
@@ -0,0 +1,11 @@
+import "./Body.css";
+
+function Body() {
+  return(
+    <body>
+      Wowd
+    </body>
+  );
+}
+
+export default Body;
