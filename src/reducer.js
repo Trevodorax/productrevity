1e8002527ddb7b2f4b03c2c191daac953cd1a6df
Paul Gaudeaux <pgx@MacBook-MF7PTYG9Y9-PGAUDEAUX.local>
2022-09-11 16:36:52 +0200

    turned store into an object with initial value (and necessary modifications in subs

--- src/reducer.js
+++ src/reducer.js
@@ -1,9 +1,13 @@
-export const countReducer = (state = 0, action) => {
+const initialState = {
+    count: 0,
+}
+
+export const countReducer = (state = initialState, action) => {
     switch(action.type) {
         case "INCREMENT" :
-            return state + 1;
+            return {...initialState, count: state.count + 1};
         case "DECREMENT" :
-            return state - 1;
+            return {...initialState, count: state.count - 1};
         default :
             return state;
     };
--- src/routes/ListsRoute/NbListsControls.js
+++ src/routes/ListsRoute/NbListsControls.js
@@ -14,7 +14,7 @@ const UnconnectedNbListsControl = (props) => {
 
 const mapStateToProps = state => {
     return {
-        count: state
+        count: state.count
     };
 };
 
